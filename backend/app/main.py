10a1e8397b15a074d5214c7cccd7689f32436f73
AG3NTZ3R0 <Z3R0@duck.com>
2023-11-25 16:27:27 -0500

    changes name of files from budgets.py to budget.py

--- backend/app/main.py
+++ backend/app/main.py
@@ -2,9 +2,9 @@
 Main Application File
 """
 from fastapi import FastAPI
-from routers import budgets, root
+from routers import budget, root
 
 app = FastAPI()
 
 app.include_router(root.router)
-app.include_router(budgets.router)
+app.include_router(budget.router)
